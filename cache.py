97e9977a8c25c01cc43b59bcc1ab038a36856800
rkelkka <raine_kelkka@hotmail.com>
2023-08-27 14:15:04 +0300

    Improved cache usage logging

--- cache.py
+++ cache.py
@@ -19,13 +19,13 @@ def _get_cache_file_path(input_files):
     return os.path.join(CACHE_DIR_REL_TO_WD, _digest(input_files) + ".json")
 
 def _write(path, data):
-    logger.info("Writing cached metadata: %s", path)
+    logger.debug("Writing cached metadata: %s", path)
     _ensure_cache_dir_exists()
     with open(path, "w+") as fp:
         json.dump(data, fp)
 
 def _read(path):
-    logger.info("Reading cached metadata: %s", path)
+    logger.debug("Reading cached metadata: %s", path)
     if os.path.isfile(path):
         with open(path, "r") as fp:
             return json.load(fp)
@@ -36,8 +36,11 @@ def with_cache(input_files, reader_fn):
     cache_file_path = _get_cache_file_path(input_files)
     cached = _read(cache_file_path)
     if (cached):
+        logger.info("Found cached metadata: %s", cache_file_path)
         return cached
     else:
+        logger.info("Cached metadata not found, reading metadata from files. This can take sevaral seconds for large number of images.")
         data = reader_fn(input_files)
+        logger.info("Read done, writing cached metadata: %s", cache_file_path)
         _write(cache_file_path, data)
         return data
